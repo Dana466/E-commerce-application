e04827ad364977f9e8b00c7138cb1c33a5dbe531
Dana466 <merhidana466@gmail.com>
2024-09-13 16:27:17 +0300

    navigationMenu.tsx

--- frontend/components/navigationMenu.tsx
+++ frontend/components/navigationMenu.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { logoutUser } from '../utils/firebaseauthfcts.ts';
 import searchimage from '../assests/search.png';
 import heartimage from '../assests/heart.png';
 import cardimage from '../assests/cart.png';
@@ -18,20 +17,6 @@ const Nav: React.FC<NavProps> = ({ pageName })=> {
     navigate('/profile'); 
   };
 
-  const handleSignout = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const userConfirmed = window.confirm('Are you sure you want to logout?');
-  
-    if (userConfirmed) {
-      try {
-        await logoutUser(); 
-      
-      } catch (error) {
-        console.error('Error logging out:', error);
-      }
-    }
-  };
   
 
   return (
@@ -76,21 +61,18 @@ const Nav: React.FC<NavProps> = ({ pageName })=> {
 const MobileNav = () => {
   return (
     <nav className="mobile_nav mobile_nav_hide">
-      <ul className="mobile_nav_list">
+        <ul className="mobile_nav_list">
         <li className="mobile_nav_item">
           <a href="/shop" className="mobile_nav_link">Home</a>
         </li>
         <li className="mobile_nav_item">
-          <a href="/store" className="mobile_nav_link">store</a>
+          <a href="/register" className="mobile_nav_link">SignUp</a>
         </li>
         <li className="mobile_nav_item">
-          <a href="/forget-password" className="mobile_nav_link">Contact</a>
+          <a href="/contact" className="mobile_nav_link">Contact</a>
         </li>
         <li className="mobile_nav_item">
-          <a href="/sign-up.html" className="mobile_nav_link">Sign Up</a>
-        </li>
-        <li className="mobile_nav_item">
-          <a href="/cart.html" className="mobile_nav_link">Cart</a>
+          <a href="/about" className="mobile_nav_link">About</a>
         </li>
       </ul>
     </nav>
