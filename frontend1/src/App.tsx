7fa5b545ed4942c5c25e34bd86d42ad508bd78eb
Dana466 <merhidana466@gmail.com>
2024-09-13 17:10:47 +0300

    App.tsx

--- frontend1/src/App.tsx
+++ frontend1/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from 'react';
 import './App.css';
 import Register from './pages/register.tsx';
@@ -6,8 +5,6 @@ import Login from './pages/login.tsx';
 import { selectUsers } from './storeredux/userslice.ts';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import Shop from './pages/home.tsx';
-import ProductList from './pages/shop.tsx';
 import PasswordReset from './pages/resetpassword.tsx';
 import Profile from './pages/profile.tsx';
 import HomePage from './pages/homepage.tsx';
@@ -15,7 +12,7 @@ import Cart from './pages/cart.tsx';
 import Checkout from './pages/checkout.tsx';
 import About from './pages/about.tsx';
 import Contact from './pages/contact.tsx';
-
+import DeleteAccount from './pages/deleteacc';
 import { CartProvider } from './context/cartcontext.tsx'; // Import CartProvider
 
 function App() {
@@ -28,8 +25,6 @@ function App() {
           <>
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
-            <Route path='/shop' element={<Shop />} />
-            <Route path='/store' element={<ProductList />} />
             <Route path='/forget-password' element={<PasswordReset />} />
             <Route path='/profile' element={<Profile />} />
             <Route path='/homepage' element={<HomePage />} />
@@ -37,7 +32,7 @@ function App() {
             <Route path='/checkout' element={<Checkout />} />
             <Route path='/about' element={<About />} />
             <Route path='/contact' element={<Contact />} />
-
+ <Route path='/deleteaccount' element={<DeleteAccount/>}/>
 
           </>
           <>
